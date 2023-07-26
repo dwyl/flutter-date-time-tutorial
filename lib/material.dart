bccc31aff6d8e5e313165f4fbad33f265dd77f2a
Luís Arteiro <lucho14garfield@gmail.com>
2023-07-26 16:12:33 +0100

    feat: Add tests for Material. #1

--- lib/material.dart
+++ lib/material.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+final dateButtonKey = UniqueKey();
+final timeButtonKey = UniqueKey();
+final dateTimeButtonKey = UniqueKey();
+
 /// Material example page.
 /// Showcases the usage of `DatePicker` and `TimePicker` to change date and time.
 class MaterialExamplePage extends StatefulWidget {
@@ -69,6 +73,7 @@ class _MaterialExamplePageState extends State<MaterialExamplePage> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   ElevatedButton(
+                      key: dateButtonKey,
                       style: ElevatedButton.styleFrom(backgroundColor: Colors.blue.shade300),
                       child: const Text(
                         "Date",
@@ -85,6 +90,7 @@ class _MaterialExamplePageState extends State<MaterialExamplePage> {
                         );
                       }),
                   ElevatedButton(
+                      key: timeButtonKey,
                       style: ElevatedButton.styleFrom(backgroundColor: Colors.blue.shade300),
                       child: const Text(
                         "Time",
@@ -109,6 +115,7 @@ class _MaterialExamplePageState extends State<MaterialExamplePage> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   ElevatedButton(
+                      key: dateTimeButtonKey,
                       style: ElevatedButton.styleFrom(backgroundColor: Colors.orange.shade400),
                       onPressed: pickDateTime,
                       child: const Text(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:app/material.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -15,7 +16,6 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const App());
 
-    // Verify that our counter starts at 0.
     expect(find.byKey(materialPageKey), findsOneWidget);
     expect(find.byKey(cupertinoPageKey), findsNothing);
 
@@ -25,5 +25,59 @@ void main() {
 
     expect(find.byKey(cupertinoPageKey), findsOneWidget);
     expect(find.byKey(materialPageKey), findsNothing);
+  });
+
+  testWidgets('Test Material page pickers', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const App());
+
+    expect(find.byKey(materialPageKey), findsOneWidget);
+    expect(find.byKey(cupertinoPageKey), findsNothing);
+
+    // TEST DATE BUTTON ONLY -----------
+    final dateButton = find.byKey(dateButtonKey);
+
+    // Tap on `Cupertino` key and page should change
+    await tester.tap(dateButton);
+    await tester.pumpAndSettle();
+
+    // Tap on "OK"
+    await tester.tap(find.text('OK'));
+    await tester.pumpAndSettle();
+
+    // Should show page again
+    expect(find.byKey(materialPageKey), findsOneWidget);
+
+    // TEST TIME BUTTON ONLY -----------
+    final timeButton = find.byKey(timeButtonKey);
+
+    // Tap on `Cupertino` key and page should change
+    await tester.tap(timeButton);
+    await tester.pumpAndSettle();
+
+    // Tap on "OK"
+    await tester.tap(find.text('OK'));
+    await tester.pumpAndSettle();
+
+    // Should show page again
+    expect(find.byKey(materialPageKey), findsOneWidget);
+
+    // TEST DATE AND TIME BUTTON -----------
+    final dateTimeButton = find.byKey(dateTimeButtonKey);
+
+    // Tap on `Cupertino` key and page should change
+    await tester.tap(dateTimeButton);
+    await tester.pumpAndSettle();
+
+    // Tap on "OK" for date
+    await tester.tap(find.text('OK'));
+    await tester.pumpAndSettle();
+
+    // Tap on "OK" for time
+    await tester.tap(find.text('OK'));
+    await tester.pumpAndSettle();
+
+    // Should show page again
+    expect(find.byKey(materialPageKey), findsOneWidget);
   });
 }
